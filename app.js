10cc2ab6c7c699a8162ec381dc1a2e881310dd1a
hahmed1523 <hahmed1523@gmail.com>
2022-07-26 14:50:45 -0400

    Add express routers for users and tweets

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
-
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = require('./config/keys').mongoURI;
@@ -10,7 +12,11 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello Mon!"));
+app.get("/", (req, res) => res.send("Hello !"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
