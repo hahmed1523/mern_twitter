57398318ba4b9c17c0b17f4d83c1c6ad4afa0a62
hahmed1523 <hahmed1523@gmail.com>
2022-07-28 16:02:36 -0400

    Add redux libraries

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import configureStore from './store/store';
 import App from './App';
 import axios from "axios";
 
+document.addEventListener("DOMContentLoaded", () => {
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  
+  window.axios = axios;
+  const store = configureStore();
+  window.store = store;
 
-window.axios = axios;
+})
+
+
 
