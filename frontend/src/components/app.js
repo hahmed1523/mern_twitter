06e3e4ddc8d497836c6679e914f2da0ef0861c71
hahmed1523 <hahmed1523@gmail.com>
2022-07-29 18:31:07 -0400

    Complete Navbar component

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Routes,Route } from 'react-router-dom';
 import MainPage from './main/main_page';
+import NavbarContainer from './nav/navbar_container';
 
 const App = () => (
     <div className='page-container'>
         
+        <NavbarContainer />
+
         <Routes>
             <Route path="/" element={<MainPage />} />
 
---
+++ frontend/src/components/nav/navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.logoutUser = this.logoutUser.bind(this);
+        this.getLinks = this.getLinks.bind(this);
+    }
+
+    logoutUser(e) {
+        e.preventDefault();
+        this.props.logout();
+    }
+
+    getLinks() {
+        if (this.props.loggedIn) {
+            return (
+                <div className='nav-items'>
+                    <Link to={'/tweets'}>All Tweets</Link>
+                    <Link to={'/profile'}>Profile</Link>
+                    <Link to={'/new_tweet'}>All Tweets</Link>
+                    <button onClick={this.logoutUser}>Logout</button>
+                </div>
+            );
+        } else {
+            return (
+                <div className='nav-items'>
+                    <Link to={'/signup'}>Signup</Link>
+                    <Link to={'/login'}>Login</Link>
+                </div>
+            );
+        };
+    }
+
+    render() {
+        return (
+            <nav>
+                { this.getLinks() }
+            </nav>
+        );
+    }
+
+};
+
+export default NavBar;
