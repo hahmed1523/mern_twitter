e6ba25ef80da576531cc4e061f5398bf13edd56d
hahmed1523 <hahmed1523@gmail.com>
2022-08-02 13:19:46 -0400

    Add tweets_errors_reducer.js

---
+++ frontend/src/reducers/tweets_errors_reducer.js
@@ -0,0 +1,13 @@
+import { RECEIVE_TWEETS, RECEIVE_USER_TWEETS, RECEIVE_NEW_TWEET, RECEIVE_TWEET_ERRORS } from "../actions/tweet_actions";
+
+const _nullErrors = [];
+
+const TweetsErrorsReducer = (state = _nullErrors, action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_TWEET_ERRORS:
+            return action.errors;
+        default:
+            return _nullErrors;
+    }
+};
